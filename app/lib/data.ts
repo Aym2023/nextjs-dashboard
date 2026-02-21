e10d2db5f809a6130e21c2fcadf60c77c13e2f0c
ayman <ayman.sale000@gmail.com>
2026-02-21 16:06:37 +0200

    dashboard: add loading state to latest invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -63,7 +63,13 @@ const sql = postgres(connectionString, {
 
 export async function fetchRevenue() {
   try {
+    //  console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+
+    //  console.log('Data fetch completed after 3 seconds.');
+
     return data;
   } catch (error: any) {
     console.error('Database Error in fetchRevenue:', error);
